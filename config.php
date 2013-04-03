7d66b4ed7edc9ca3daa3f0c4ed36f9e84e733b39
jooskim <jooskim@umich.edu>
2013-04-03 14:26:49 -0400

    bug fix

--- config.php
+++ config.php
@@ -4,14 +4,6 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->adminpw = 'changeme';
-
-$CFG->database  = 'csonline';
-$CFG->dbhost    = '127.0.0.1';
-$CFG->dbport    = '8889';
-$CFG->dbuser    = 'root';
-$CFG->dbpass    = 'root';
-
 // Cookie parameters
 $CFG->cookiesecret = 'dj21jt848s9f9ah2hhhtt13was8491rhq9fsuh93';
 $CFG->cookiename = 'identity';
--- signup.php
+++ signup.php
@@ -14,15 +14,17 @@ dbconnect();
 $query = "SELECT id FROM CT_User WHERE userEmail = '$Email';";
 $result = mysql_query($query, $connect);
 if(!$result){
-	die("DB transaction error!");
+	$_SESSION['error'] = "DB transaaction error!";
+	header("Location: index.php");
 }else{
 	if(mysql_num_rows($result) > 0){
-		die("There is already an account with the same email account!");
+		$_SESSION['error'] = "There is already an account with the same email account!";
 		header("Location: index.php");
 	}else{
 		$query = "INSERT INTO CT_User (userEmail, userPw, firstName, lastName, joinDate, emailSHA) VALUES('$Email', MD5('$Pw'), '$firstName', '$lastName', NOW(), SHA1('$Email'));";
 		if(!($result = mysql_query($query, $connect))){
-			die("DB insert error!");
+			$_SESSION['error'] = "DB insert error!";
+			header("Location: index.php");
 		}else{
 			$_SESSION['newId'] = mysql_insert_id($connect);
 			$_SESSION['userPw'] = md5($Pw);
